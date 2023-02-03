ff03b27ac1cf71537385057404be6962ad06740d
Nikola Borska <m-nikol.svedova@email.cz>
2023-02-03 18:14:22 +0100

    vypis js

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 // tady je místo pro náš program
 
-let jmeno = prompt("Napiš své jméno a příjmení: ");
-jmeno.innerHTML = jmeno;
+//jmeno + prijmeni uzivatele
+let jmeno = document.querySelector(".jmeno");
+let jmenoUzivatele = prompt("Zadej své celé jméno: ");
 
+//jmeno.innerHTML = jmenoUzivatele;
+
+//vek uzivatele
+let vek = document.querySelector(".vek");
+let rok = 2023;
+let vekUzivatele = Number(prompt("Zadej svůj věk: "));
+let vypocetVeku = (rok - vekUzivatele);
+
+//vek.innerHTML = vypocetVeku;
+
+//vypis jmena, prijmeni a vek 
+document.body.innerHTML += "<p>" + jmenoUzivatele + " " + vypocetVeku + "</p>";
+
+//oblibena barva uzivatele
+let barva = document.querySelector(".barva");
+let barvaUzivatele = prompt("Zadej svou oblíbenou barvu v anglickém jazyce: ");
+
+barva.innerHTML = barvaUzivatele;
+
+//prebarveny text
+jmenoUzivatele.style.color = "barvaUzivatele";
 
